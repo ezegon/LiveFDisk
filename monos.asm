d44a65bb2c95d3436ff8328850d93d811c880f0f
noobgamer <ezegon.lcc@gmail.com>
2018-01-16 14:58:10 -0300

    Added hdd

--- monos.asm
+++ monos.asm
@@ -32,33 +32,40 @@ loop_menu:
 
 ;----------------------------------!
 general:
-    xor ax, ax ;Clear ax
-    mov ah, 0x02 ;Read sectors
-    mov al, 1 ;Read one sector
-    mov ch, 0 ;Cylinder 0
-    mov cl, 1 ;First Sector
-    mov dh, 0 ;Head 0
-    mov dl, 0x80 ;HDD #0
-    mov bx, 0x9000 ;Save buffer in 0x9000
-    mov es, bx
-    xor bx,bx ;Clear bx
-    int 0x13 ;Drives interrupt
-    jc reading_error
-    
-    mov ah, 0x03
-    mov si, header
-    call print_string
-    
-read_sectors:
-    mov bx, 0x01BE
-    inc byte [partition_count] ;Update count
-    cmp byte [partition_count], 5; Check if last partition
-    mov si, [es:bx]
-    
-
-reading_error:
-
-
+     mov dx, 0x01F2
+     in ax, dx
+     mov si, di
+     call print_string
+;    xor ax, ax ;Clear ax
+;    mov ah, 0x02 ;Read sectors
+;    mov al, 1 ;Read one sector
+;    mov ch, 0 ;Cylinder 0
+;    mov cl, 1 ;First Sector
+;    mov dh, 0 ;Head 0
+;    mov dl, 0x80 ;HDD #0
+;    mov bx, 0x9000 ;Save buffer in 0x9000
+;    mov es, bx
+;    xor bx,bx ;Clear bx
+;    int 0x13 ;Drives interrupt
+;    jc reading_error
+;    
+;    mov ah, 0x03
+;    mov si, header
+;    call print_string
+;    
+;read_sectors:
+;    mov bx, 0x01BE
+;    inc byte [partition_count] ;Update count
+;    cmp byte [partition_count], 5; Check if last partition
+;    mov byte si, [es:bx]
+;    ;call print_string
+;    
+;
+;reading_error:
+;    mov si, errorreading
+;    call print_string
+;    jmp loop_menu
+;
 ;----------------------------------¡
 
 ;----------------------------------!
@@ -112,6 +119,7 @@ column db 0
 ;Messages
 welcome db "Welcome to monOS",0
 option1 db "1-Show partition table",0
+errorreading db "Error reading from disk",0
 header db "Partition N          Status          Partition Type          Number of sectors"
     
 ;END NO TOCAR
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ echo "Welcome to monOS install"
 
 nasm -f bin -o monos.bin monos.asm
 
-dd status=noxfer conv=notrunc if=monos.bin of=monos.qcow
+dd status=noxfer conv=notrunc if=monos.bin of=myimage.img
 
-qemu-system-i386 -s -hda monos.qcow
+qemu-system-i386 myimage.img -hdb fat:./hdd
