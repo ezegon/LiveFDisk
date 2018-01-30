25e8263414c0f732de775af0b2030824b565a6b9
noobgamer <ezegon.lcc@gmail.com>
2018-01-30 15:37:12 -0300

    Major bug fix

--- loader.asm
+++ loader.asm
@@ -7,7 +7,7 @@ loader:
     mov ah, 0x02 ;Read sectors
     mov al, 4 ;Read four sectors
     mov ch, 0 ;Cylinder 0
-    mov cl, 1 ;First Sector
+    mov cl, 2 ;First Sector
     mov dh, 0 ;Head 0
     mov dl, 0 ;Floppy
     mov bx, 0x8000 ;Save buffer in 0x8000
@@ -15,6 +15,8 @@ loader:
     xor bx,bx ;Clear bx
     int 0x13 ;Drives interrupt
     jc reading_error
+    mov bx, 0x8000
+    mov ds, bx
     jmp 0x8000:0
     
 reading_error:
