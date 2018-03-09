1295bdd6e44cc9881ce8ee9350b23e721e87c42e
unknown <gonzanob99@gmail.com>
2018-03-08 21:44:56 -0300

    wip

--- mvideo.asm
+++ mvideo.asm
@@ -21,6 +21,34 @@ loop_rcfs:
     add bx, 2
     jmp loop_rcfs
     
-
+print_char_at_pos:
+	pusha
+	xor bx, bx
+	
+	mov al, 80
+	mul si
+	add ax, 0xb800
+	mov es, ax
+	mov al, 2
+	mul di
+	mov bx, ax
+	mov al, dl
+	mov ah, 0x40
+	mov [es:bx], ax
+	
+	jmp generic_end
+	
 clearscreen:
     pusha
+	mov cx, 80
+	
+loop_clearscreen:
+	mov ax, 0xb800
+    mov es, ax
+	mov al, 'A'
+	mov ah, 0x40
+    mov [es:bx], ax
+    add bx, 2
+    loop loop_clearscreen
+	
+	jmp generic_end
